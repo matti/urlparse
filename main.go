f6e88af3d368d1a2d21595cca5a8b63020541e7a
Matti Paksula <matti.paksula@iki.fi>
2022-09-12 12:07:27 +0300

    better

--- main.go
+++ main.go
@@ -14,20 +14,37 @@ func main() {
 	if u, err := url.Parse(urlString); err != nil {
 		panic(err)
 	} else {
+		if u.Scheme == "" {
+			u.Scheme = "http"
+			if u, err = url.Parse(u.String()); err != nil {
+				panic(err)
+			}
+		}
+
 		password := ""
 		if p, ok := u.User.Password(); ok {
 			password = p
 		}
 
+		port := u.Port()
+		if port == "" {
+			switch u.Scheme {
+			case "http":
+				port = "80"
+			case "https":
+				port = "443"
+			}
+		}
+
 		parts := []string{
-			u.Scheme,
-			u.User.Username(),
-			password,
-			u.Host,
-			u.Port(),
-			u.Path,
-			u.Fragment,
-			u.RawQuery,
+			"scheme=" + u.Scheme,
+			"user=" + u.User.Username(),
+			"password=" + password,
+			"host=" + u.Host,
+			"port=" + port,
+			"path=" + u.Path,
+			"fragment=" + u.Fragment,
+			"query=" + u.RawQuery,
 		}
 
 		fmt.Println(strings.Join(parts, "\n"))
